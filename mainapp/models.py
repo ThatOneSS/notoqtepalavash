1829d55d241b723ec08703808002fbb1b1092929
ThatOneSS <saidakbarsamadov1@gmail.com>
2025-06-11 14:44:27 +0500

    Images again...

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image= models.CharField(blank=True , null=True)
+    image= models.TextField(blank=True , null=True)
     status= models.BooleanField(default=True)
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image= models.CharField(blank=True , null=True)
+    image= models.TextField(blank=True , null=True)
 
     status = models.BooleanField(default=True)
 
@@ -24,7 +24,7 @@ class Product(models.Model):
 class Branch(models.Model):
     name = models.CharField(max_length=100, unique=True)
     working_hours = models.CharField(max_length=100, default='9:00 - 22:00 PM')
-    image= models.CharField(blank=True , null=True)
+    image= models.TextField(blank=True , null=True)
     address=models.URLField()
 
     status = models.BooleanField(default=True)
--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Category, Product , Branch
 
+
 class ProductSerializer(serializers.ModelSerializer):
+    category=serializers.StringRelatedField()
     class Meta:
         model = Product
         fields = ['id', 'category', 'name', 'image','price','description']
-
 
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
